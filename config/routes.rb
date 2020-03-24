19fcadef41c76cdc8076a5b5aa3d855e8a843701
Andrew Reid <areid.sj@gmail.com>
2020-03-24 16:36:36 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,22 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
-  
-  post '/shelters/:id', to: 'reviews#create'
-
-
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-  get '/shelters/:id/pets', to: 'showpets#index'
+
+  get '/pets', to: 'pets#index'
   get '/shelters/:id/pets/new', to: 'pets#new'
   post '/shelters/:id/pets', to: 'pets#create'
-  get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
   patch '/pets/:id', to: 'pets#update'
   delete '/pets/:id', to: 'pets#destroy'
+
+    get '/shelters/:id/pets', to: 'showpets#index'
+
   get '/shelters/:id/review/new', to: 'reviews#new'
+  post '/shelters/:id', to: 'reviews#create'
 end
