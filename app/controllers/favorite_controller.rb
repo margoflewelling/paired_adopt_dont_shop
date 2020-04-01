691c9c670458fc871a61e08a269341560fce8d4d
Andrew Reid <areid.sj@gmail.com>
2020-04-01 07:32:06 -0600

    Refactor Favorite Controller

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,4 +1,8 @@
 class FavoriteController < ApplicationController
+  def index
+    @favorites = favorite.favorite_pets
+    @pets = Pet.all
+  end
 
   def update
     pet = Pet.find(params[:pet_id])
@@ -8,25 +12,24 @@ class FavoriteController < ApplicationController
     redirect_to "/pets/#{params[:pet_id]}"
   end
 
-  def index
-    @favorites = favorite.favorite_pets
-    @pets = Pet.all
-  end
-
   def destroy
     pet = Pet.find(params[:pet_id])
     favorite.favorite_pets.delete(params[:pet_id])
     session[:favorite] = favorite.favorite_pets
     flash[:notice] = "#{pet.name} has been removed from your favorites"
-    if params[:state] == "index_redirect"
-      redirect_to "/favorite"
-    else
-      redirect_to "/pets/#{params[:pet_id]}"
-    end
+    redirect_after_destroy(params)
   end
 
   def destroy_all
     session[:favorite] = []
     redirect_to "/favorite"
+  end
+
+  def redirect_after_destroy(params)
+    if params[:state] == "index_redirect"
+      redirect_to "/favorite"
+    else
+      redirect_to "/pets/#{params[:pet_id]}"
+    end
   end
 end
